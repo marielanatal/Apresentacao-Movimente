ca5e2ec4cb7bd1343fd44707c83220e37fedd089
marielanatal <mariela.natal@gmail.com>
2025-12-09 09:06:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import faturamento
 import Despesas
-import resultado_comparativo as resultado
+import resultado
 
 st.set_page_config(page_title="Dashboard Financeiro", layout="wide")
 
