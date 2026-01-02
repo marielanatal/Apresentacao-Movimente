86e2dd5a4a142d8f3cc094d4c792402b5a547f38
marielanatal <mariela.natal@gmail.com>
2026-01-02 09:46:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import streamlit as st
 import resultado
+import faturamento
+import despesa
 
 st.set_page_config(page_title="Apresentação", layout="wide")
 
 st.title("📌 Visão Geral do Ano")
 
-resultado.render()
+aba = st.sidebar.radio(
+    "Menu",
+    ["Resultado", "Faturamento", "Despesas"]
+)
 
+if aba == "Resultado":
+    resultado.render()
+
+elif aba == "Faturamento":
+    faturamento.render()
+
+elif aba == "Despesas":
+    despesa.render()
